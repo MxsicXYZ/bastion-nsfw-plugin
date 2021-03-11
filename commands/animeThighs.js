d6104c674258a8d8876ed0730f810c50ea334a94
NightRaven <53287573+Xynnix@users.noreply.github.com>
2021-03-11 04:17:41 -0500

    Create animeThighs.js

---
+++ commands/animeThighs.js
@@ -0,0 +1,42 @@
+/*!
+ * @plugin bastion-example-plugin
+ * @author Sankarsan Kampa (k3rn31p4nic)
+ * @license MIT
+ */
+
+"use strict";
+
+const tesseract = require("@bastion/tesseract");
+const fetch = require("node-fetch");
+
+class animeThighsCommandPlugin extends tesseract.Command {
+    constructor() {
+        super("animeThighs", {
+            description: "Sends an NSFW image into the channel",
+            triggers: [ "athighs" ],
+            arguments: {},
+            scope: "guild",
+            owner: false,
+            typing: true,
+            schedulable: true,
+            unsafe: false,
+            nsfw: true,
+            cooldown: 5,
+            ratelimit: 1,
+            clientPermissions: ["SEND_MESSAGES", "EMBED_LINKS"],
+            userPermissions: [],
+            syntax: [],
+            condition: () => true,
+        });
+    }
+
+    exec = async (message, argv) => {
+        await fetch("https://lewds.ga/v1/nsfw/athighs")
+                .then(res => res.json())
+                .then(json =>{
+        message.channel.send(json.result);
+        }
+    }
+}
+
+module.exports = animeThighsCommandPlugin;
