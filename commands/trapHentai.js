d04e06d740fea58aae9e6cf7b5b1dbcca57a7954
NightRaven <53287573+Xynnix@users.noreply.github.com>
2021-03-11 04:44:57 -0500

    Create trapHentai.js

---
+++ commands/trapHentai.js
@@ -0,0 +1,41 @@
+/*!
+ * @plugin bastion-nsfw-plugin
+ * @author Xanathero (@Xynnix)
+ * @license MIT
+*/
+"use strict";
+
+const tesseract = require("@bastion/tesseract");
+const fetch = require("node-fetch");
+
+class trapHentaiCommandPlugin extends tesseract.Command {
+    constructor() {
+        super("slimeHentai", {
+            description: "Sends an NSFW image into the channel",
+            triggers: ["slime"],
+            arguments: {},
+            scope: "guild",
+            owner: false,
+            typing: true,
+            schedulable: true,
+            unsafe: false,
+            nsfw: true,
+            cooldown: 5,
+            ratelimit: 1,
+            clientPermissions: ["SEND_MESSAGES", "EMBED_LINKS"],
+            userPermissions: [],
+            syntax: [],
+            condition: () => true,
+        });
+    }
+
+    exec = async (message, argv) => {
+        await fetch("https://lewds.ga/v1/nsfw/trap")
+                .then(res => res.json())
+                .then(json =>{
+        message.channel.send(json.result);
+        }
+    }
+}
+
+module.exports = trapHentaiCommandPlugin;
