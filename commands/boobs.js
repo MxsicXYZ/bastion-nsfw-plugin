206ab97006fba1f7618b5c5057d2e2ae8842ca73
NightRaven <53287573+Xynnix@users.noreply.github.com>
2021-03-11 04:11:47 -0500

    Create boobs.js

---
+++ commands/boobs.js
@@ -0,0 +1,42 @@
+/*!
+ * @plugin bastion-example-plugin
+ * @author Sankarsan Kampa (k3rn31p4nic)
+ * @license MIT
+ */
+
+"use strict";
+
+const tesseract = require("@bastion/tesseract");
+const fetch = require("node-fetch");
+
+class boobsCommandPlugin extends tesseract.Command {
+    constructor() {
+        super("boobs", {
+            description: "Sends an NSFW image into the channel",
+            triggers: [ "breasts", "tits" ],
+            arguments: {},
+            scope: "guild",
+            owner: false,
+            typing: true,
+            schedulable: true,
+            unsafe: false,
+            nsfw: true,
+            cooldown: 5,
+            ratelimit: 1,
+            clientPermissions: ["SEND_MESSAGES", "EMBED_LINKS"],
+            userPermissions: [],
+            syntax: [],
+            condition: () => true,
+        });
+    }
+
+    exec = async (message, argv) => {
+        await fetch("https://lewds.ga/v1/nsfw/boobs")
+                .then(res => res.json())
+                .then(json =>{
+        message.channel.send(json.result);
+        }
+    }
+}
+
+module.exports = boobsCommandPlugin;
