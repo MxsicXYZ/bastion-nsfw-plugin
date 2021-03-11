2dd413d6dac21d3536f73d2a4f92ec0ccb15351e
NightRaven <53287573+Xynnix@users.noreply.github.com>
2021-03-11 04:39:53 -0500

    Create milkHentai.js

---
+++ commands/milkHentai.js
@@ -0,0 +1,42 @@
+  
+/*!
+ * @plugin bastion-nsfw-plugin
+ * @author Xanathero (@Xynnix)
+ * @license MIT
+*/
+"use strict";
+
+const tesseract = require("@bastion/tesseract");
+const fetch = require("node-fetch");
+
+class milkHentaiCommandPlugin extends tesseract.Command {
+    constructor() {
+        super("milkHentai", {
+            description: "Sends an NSFW image into the channel",
+            triggers: ["milk"],
+            arguments: {},
+            scope: "guild",
+            owner: false,
+            typing: true,
+            schedulable: true,
+            unsafe: false,
+            nsfw: true,
+            cooldown: 5,
+            ratelimit: 1,
+            clientPermissions: ["SEND_MESSAGES", "EMBED_LINKS"],
+            userPermissions: [],
+            syntax: [],
+            condition: () => true,
+        });
+    }
+
+    exec = async (message, argv) => {
+        await fetch("https://lewds.ga/v1/nsfw/milk")
+                .then(res => res.json())
+                .then(json =>{
+        message.channel.send(json.result);
+        }
+    }
+}
+
+module.exports = milkHentaiCommandPlugin;
